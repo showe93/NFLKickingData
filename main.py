fce7dfae974e8e1c56c796049bde95bd49c0e218
Sean Howe <showe1364@gmail.com>
2024-02-15 15:46:06 -0500

    adds function that finds the total percentage of field goals made vs field goals attempted and returns the value as a percentage

--- main.py
+++ main.py
@@ -1,12 +1,14 @@
-from sortTable import player_names, fgm_total
+from sortTable import player_names, fgm_total, fgm_vs_fga
 import sqlite3
 
 if __name__ == '__main__':
     try:
         connection = sqlite3.connect("Data/Database/NFL_Kicking_Data.sqlite")
         cursor = connection.cursor()
-        player_names(cursor.execute('SELECT * FROM "main"."2023KickingData"'))
-        fgm_total(cursor.execute('SELECT * FROM "main"."2023KickingData"'))
+      #  player_names(cursor.execute('SELECT * FROM "main"."2023KickingData"'))
+      #  fgm_total(cursor.execute('SELECT * FROM "main"."2023KickingData"'))
+        percentageMade = fgm_vs_fga(cursor.execute('SELECT * FROM "main"."2023KickingData"'))
+        print(percentageMade)
         cursor.close()
     except sqlite3.Error as db_error:
         print(f'A Database Error has occurred: {db_error}')
--- sortTable.py
+++ sortTable.py
@@ -1,6 +1,5 @@
 """This file will initially confirm that our data is read correctly and will be used for future reference"""
 
-
 """This function will check that the player names can be returned in a loop one at a time and in order"""
 
 
@@ -14,8 +13,23 @@ order"""
 
 
 def fgm_total(data):
-    totalyards = 0
+    totalAttempts = 0
     for row in data:
-        print(row[13])
-        totalyards = totalyards + int(row[13])
-    print(totalyards)
+        print(row[12])
+        totalAttempts = totalAttempts + int(row[12])
+    print(totalAttempts)
+
+
+"""This function will find the probability of a kicker making a field goal vs total field goal attempts across the 
+league for the entire 2023-2024 NFL season"""
+
+
+def fgm_vs_fga(data):
+    totalAttempts = 0
+    totalMade = 0
+    for row in data:
+        totalAttempts = totalAttempts + int(row[12])
+        totalMade = totalMade + int(row[13])
+    percentageMade = totalMade / totalAttempts
+    percentageMade = f"{percentageMade: .2%}"
+    return percentageMade
